a4241c3d4e81486b0a4ef09e41948e07aae80b94
osanrichy <osanwawe@gmail.com>
2022-09-12 14:19:05 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <studio.h>
 /**
- * main - print the string in the put function
- * 
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Return: 0
+ * main - Printing
+ *
+ * Return: Always 0(Successful)
  */
 int main(void)
 {
