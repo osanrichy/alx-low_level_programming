e9797aec162c79beec8d06080f80255e3839a517
osanrichy <osanwawe@gmail.com>
2022-09-12 15:17:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Return: Always 0(successful)
+ * this program prints "programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
